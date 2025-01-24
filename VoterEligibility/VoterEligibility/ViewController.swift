c1dcf3feac0bd07771d91df9f8363979bd29c5ab
Owen Graham <omgraham02@gmail.com>
2025-01-23 19:03:44 -0600

    Added VoterEligibility program

---
+++ VoterEligibility/VoterEligibility/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoterEligibility
+//
+//  Created by Owen Graham on 1/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
