abcb50bad8c433f59b67a736482737de15a66963
Owen Graham <omgraham02@gmail.com>
2025-01-28 09:59:39 -0600

    Updated VoterEligibility

--- VoterEligibility/VoterEligibility/ViewController.swift
+++ VoterEligibility/VoterEligibility/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var photo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var age = textInput.text
+        
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if inputText >= 18 {
+            text.print("Eligible to Vote!")
+        }
+        else {
+            text.print("Sorry, you cannot vote")
+        }
+    }
 }
 
