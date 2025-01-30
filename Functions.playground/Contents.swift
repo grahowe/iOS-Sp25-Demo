4a2887b51ac552473ea2776c7bf43a000cf69236
Owen Graham <omgraham02@gmail.com>
2025-01-30 11:59:40 -0600

    Added more functions

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -3,3 +3,37 @@ func greetUser(){
 }
 
 greetUser()
+
+
+func favoriteProgram(name:String)->String {
+    var name = "My favorite programming language is \(name)"
+    return name
+}
+print(favoriteProgram(name: "Python"))
+
+
+func addNumbers(number1:Int, number2:Int)->Int {
+    return number1+number2
+}
+print("The sum of the numbers is \(addNumbers(number1: 10, number2: 20))")
+
+
+func sumAndAvg(_ numbers:Double...)->(sum:Double,avg:Double,diff:Double) {
+    var total:Double = 0
+    var totaldiff:Double = 0
+    var avg:Double
+    for number in numbers {
+
+        total += number
+        totaldiff -= number
+    }
+    avg=total/Double(numbers.count)
+    return (total,avg,totaldiff)
+}
+
+let result = sumAndAvg(1, 20.5, 3, 4, 10.9)
+print("Sum = \(result.sum)")
+print("Average = \(result.avg)")
+print("Difference = \(result.diff)")
+
+
