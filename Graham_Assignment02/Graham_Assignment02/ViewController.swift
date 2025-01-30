3281044750da1e2d44bf2f4935b46de10fcce496
Owen Graham <omgraham02@gmail.com>
2025-01-30 12:11:11 -0600

    Added files for Assignment02

---
+++ Graham_Assignment02/Graham_Assignment02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Graham_Assignment02
+//
+//  Created by Graham,Owen M on 1/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
